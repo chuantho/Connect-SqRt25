30a330d249813fb41c3bf43a73d39f6519efe136
chuantho <47635966+chuantho@users.noreply.github.com>
2019-02-17 18:15:33 -0500

    Add StopWatch Class
    The StopWatch Class calculates the duration of the match when the
    match is complete. This class will be called to determine the
    remaining time a player has before the computer places a token randomly
    on their behalf. This class returns a string for its start, stop and elapsed
    functions because connect5.py will call this class and store
    the information rather than just printing it to the view.

---
+++ Timer.py
@@ -0,0 +1,43 @@
+import datetime;
+import time;
+
+class StopWatch():
+    """
+    Created Stopwatch to calculate player's turns as well as the match time
+    """
+    def __init__(self):
+        pass
+
+    def start(self):
+        """
+        Start the timer
+        """
+        self.start_time = datetime.datetime.now()
+        return self.start   
+    
+    def stop(self):
+        """
+        Stops the timer
+        """
+        self.stop = datetime.datetime.now()
+        return message + str(self.stop - self.start)
+      
+    def elapsed(self, message="Elapsed: "):
+        """
+        Gets the elapsed time from the time start was added
+        """
+        #Return statement to integrate with the view rather than print
+        return message + str(datetime.datetime.now() - self.start_time)  
+    
+    def player_time(self): 
+        #Returns true
+        count = 0;
+        self.start_time = datetime.datetime.now()
+        #15 seconds before player loses their turn
+        while(count <=15):
+            count=count+1;
+            time.sleep(1);
+        self.stop = datetime.datetime.now()
+        return True;
+        
+
